b824d0f509895483e4088dd9909e29785ea88a56
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-04-07 06:20:39 +0900

    Web Workerでマークダウンの変換処理追加

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import * as ReactMarkdown from 'react-markdown'
 import { useStateWithStorage } from '../hooks/use_state_with_storage'
 import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
 import { SaveModal } from '../components/save_modal'
 import {Link} from 'react-router-dom'
 import {Header} from '../components/header'
-import TestWorker from 'worker-loader!../worker/test.ts' 
+import ConvertMarkdownWorker from 'worker-loader!../worker/convert_markdown_worker.ts' 
 
-const testWorker = new TestWorker()
+
+const convertMarkdownWorker = new ConvertMarkdownWorker()
 const {useState,useEffect } = React
 interface Props {
     text: string
@@ -59,18 +59,19 @@ const Preview = styled.div`
 export const Editor: React.FC<Props> = (props) => {
     const {text,setText} = props
     const [showModal, setShowModal] = useState(false)
+    const [html, setHtml] = useState('')
 
 
     // 初回のみWorkerから結果を受け取る
     useEffect(() => {
-        testWorker.onmessage = (event) => {
-            console.log('Main thread Received:', event.data)
+        convertMarkdownWorker.onmessage = (event) => {
+            setHtml(event.data.html)
         }
     }, [])
 
     // テキストの変更時にWorkerへテキストデータ送信
     useEffect(() =>{
-        testWorker.postMessage(text)
+        convertMarkdownWorker.postMessage(text)
     },[text])
 
   return (
@@ -93,7 +94,7 @@ export const Editor: React.FC<Props> = (props) => {
           value={text}
           />
         <Preview>
-            <ReactMarkdown source={text} />
+            <div dangerouslySetInnerHTML={{ __html:html}} />
         </Preview>
       </Wrapper>
       {showModal && (
